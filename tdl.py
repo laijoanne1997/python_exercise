2233551130fb87bbb4e8f790cdbcab727136b23c
laijoanne1997 <laijoanne1997@gmail.com>
2025-10-11 14:44:46 +1030

    updated

--- tdl.py
+++ tdl.py
@@ -1,2 +1,7 @@
 #creating a to-do list that can be updated and completed in python
 to_do_list=input("Would you like me to open your to-do list? (y/n) ").lowercase
+
+if to_do_list == "y":
+    file_name = input("What is your file name? ")
+    file = open(file_name)
+
