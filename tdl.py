55c8d648f05fad83f8df86890bd04cced67cac0e
laijoanne1997 <laijoanne1997@gmail.com>
2025-10-18 09:30:14 +1030

    changing .lowercase() to .lower()

--- tdl.py
+++ tdl.py
@@ -3,7 +3,7 @@
 def strike(text):
     return ''.join([c + '\u0336' for c in text])
 
-to_do_list=input("Would you like me to open your to-do list? (y/n) ").lowercase
+to_do_list=input("Would you like me to open your to-do list? (y/n) ").lower()
 
 if to_do_list == "y":
     file_name = input("What is your file name? ")
@@ -17,14 +17,13 @@ print("Here are the list of item's on your to-do list: ")
 for item in file:
     print(item)
 
-update=input("Do you want to update your to-do list? (y/n)").lowercase
+update=input("Do you want to update your to-do list? (y/n)").lower()
 
 if update =="y":
-    add_more=input("Did you want to add more (add) or complete (tick): ").lowercase.rstrip()
+    add_more=input("Did you want to add more (add) or complete (tick): ").lower().rstrip()
     if add_more == "add":
-        add = input("What else would you like to add. If you're finished, write 'done'.").lowercase()
+        add = input("What else would you like to add. If you're finished, write 'done'.").lower()
         file.write(add)
-        continue
     elif add_more == "tick":
         for item in file:
             print(item)
